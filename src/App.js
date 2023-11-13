6ea365aee3bb93dce86f0efea629eab79afb2aa1
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-13 12:13:33 +0100

    Change function Article()

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  return <Article />;
+}
+
+export function Article() {
   return (
     <article>
       <h2 className="article__title">Wir mögen Oktopoden</h2>
-      <label htmlFor="newInput"></label>
-      <input id="newInput"></input>
+      <label htmlFor="question">Question</label>
+      <input id="question"></input>
       <a
-        href="https://de.wikipedia.org/wiki/Oktopusse"
         className="article__link"
+        href="https://de.wikipedia.org/wiki/Oktopusse"
       >
         Oktopoden
       </a>
