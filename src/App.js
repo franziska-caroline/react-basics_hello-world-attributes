3aed1be7746224e00f638f3466221b72aeac0c11
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-13 11:35:03 +0100

    Complete task

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+    <article>
+      <h2 className="article__title">Wir mögen Oktopoden</h2>
+      <label htmlFor="newInput"></label>
+      <input id="newInput"></input>
+      <a
+        href="https://de.wikipedia.org/wiki/Oktopusse"
+        className="article__link"
+      >
+        Oktopoden
+      </a>
+    </article>
+  );
 }
